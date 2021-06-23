2420c8fbcaabc14512237d7fc7f9b6a50afb6fe7
Dlzheen Mahmood <dlzheenmahmood@gmail.com>
2021-06-23 21:31:05 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -2,5 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import './fetch-setup';
-
 ReactDOM.render(<App />, document.getElementById('root'));
